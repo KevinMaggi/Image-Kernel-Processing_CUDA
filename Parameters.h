f8a3163aa7ab68333f556d4aeb9a6cf6f515e73f
kevin <kevin.maggi@stud.unifi.it>
2021-11-04 20:17:03 +0100

    using of shared memory

--- Parameters.h
+++ Parameters.h
@@ -5,18 +5,29 @@
 #ifndef IMAGE_KERNEL_PROCESSING_CUDA_PARAMETERS_H
 #define IMAGE_KERNEL_PROCESSING_CUDA_PARAMETERS_H
 
-#define CONSTANT_MEMORY
-//#define SHARED_MEMORY
+/**
+ * Enable the use of constant memory in kernel for kernel
+ */
+//#define CONSTANT_MEMORY
+
+/**
+ * Enable the use of shared memory in kernel
+ */
+#define SHARED_MEMORY
+
+/**
+ * Enable the use of pinned memory for image loading
+ */
 //#define PINNED_MEMORY
 
 /**
  * Min value of kernel dimension to test (MUST be odd)
  */
-const int KERNEL_DIM_MIN = 19;
+const int KERNEL_DIM_MIN = 25;
 /**
  * Max value of kernel dimension to test (MUST be odd)
  */
-const int KERNEL_DIM_MAX = 19;
+const int KERNEL_DIM_MAX = 25;
 /**
  * Step on values of kernel dimension (MUST be even)
  */
@@ -24,7 +35,7 @@ const int KERNEL_DIM_STEP = 6;
 /**
  * Image dimension to test: 4K, 5K, 6K, 7K or 8K
  */
-const char IMAGE_DIMENSION[] = "5K";
+const char IMAGE_DIMENSION[] = "4K";
 /**
  * Number of image of each dimension to test (max 3)
  */
@@ -32,6 +43,6 @@ const int IMAGE_QUANTITY = 3;
 /**
  * Number of times to test each image
  */
-const int REPETITIONS = 1;
+const int REPETITIONS = 2;
 
 #endif //IMAGE_KERNEL_PROCESSING_CUDA_PARAMETERS_H
--- Processing.cu
+++ Processing.cu
@@ -11,7 +11,7 @@
 
 #if !defined CONSTANT_MEMORY && !defined SHARED_MEMORY
 __global__ void
-kernel(unsigned char *input, unsigned long long int *krn, unsigned char *output, int height, int width, int channels,
+kernel(unsigned char *input, const unsigned long long int *__restrict__ krn, unsigned char *output, int height, int width, int channels,
        int size, double weight) {
     int iy = blockIdx.y * blockDim.y + threadIdx.y;
     int ix = blockIdx.x * blockDim.x + threadIdx.x;
@@ -163,4 +163,206 @@ Image *process(Image *img, Kernel *krn) {
 
     return res;
 }
+#endif
+
+#if !defined CONSTANT_MEMORY && defined SHARED_MEMORY
+#define TILE_WIDTH BLOCK_SIZE
+
+__global__ void
+kernel(unsigned char *input, const unsigned long long int *__restrict__ krn, unsigned char *output, int height,
+       int width, int channels,
+       int size, double weight, int w) {
+    // shared memory
+    extern __shared__ unsigned char input_d[];
+
+    // pixel to process
+    int iy = blockIdx.y * blockDim.y + threadIdx.y;
+    int ix = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // utility values
+    int kRadius = size / 2;
+    int batches = (w * w) / (TILE_WIDTH * TILE_WIDTH);
+
+    // vars instantiation
+    int dest, destY, destX, srcY, srcX;
+    unsigned long long int newVal;
+
+    for (int ic = 0; ic < channels; ic++) {
+        // shared memory loading
+        for (int b = 0; b < batches; b++) {
+            dest = threadIdx.y * TILE_WIDTH + threadIdx.x + b * TILE_WIDTH * TILE_WIDTH;
+            destY = dest / w;
+            destX = dest % w;
+            srcY = blockIdx.y * TILE_WIDTH + destY - kRadius;
+            srcX = blockIdx.x * TILE_WIDTH + destX - kRadius;
+
+            if (destY < w) {
+                if (srcX < 0 || srcX >= width) {        // edge handling: extend
+                    srcX = (srcX < 0) ? 0 : (width - 1);
+                }
+                if (srcY < 0 || srcY >= height) {
+                    srcY = (srcY < 0) ? 0 : (height - 1);
+                }
+
+                input_d[destY * w + destX] = input[srcY * width * channels + srcX * channels + ic];
+            }
+        }
+
+        __syncthreads();
+
+        // convolution
+        if (iy < height && ix < width) {
+            newVal = 0;
+            for (int ky = 0; ky < size; ky++) {
+                for (int kx = 0; kx < size; kx++) {
+                    newVal += (unsigned long long int) input_d[(threadIdx.y + ky) * w + (threadIdx.x + kx)] *
+                              krn[ky * size + kx];
+                }
+            }
+
+            newVal = (unsigned long long int) ((long double) newVal * weight);
+            output[iy * width * channels + ix * channels + ic] = (unsigned char) newVal;
+        }
+        __syncthreads();
+    }
+}
+
+Image *process(Image *img, Kernel *krn) {
+    Image *res = Image_new_empty(img->width, img->height, img->channels);
+
+    unsigned char *d_input;
+    unsigned long long int *d_krn;
+    unsigned char *d_output;
+
+    CUDA_CHECK_RETURN(cudaMalloc((void **) &d_input, sizeof(unsigned char) * img->width * img->height * img->channels));
+    CUDA_CHECK_RETURN(cudaMalloc((void **) &d_krn, sizeof(unsigned long long int) * krn->size * krn->size));
+    CUDA_CHECK_RETURN(
+            cudaMalloc((void **) &d_output, sizeof(unsigned char) * img->width * img->height * img->channels));
+
+    CUDA_CHECK_RETURN(cudaMemcpy((void *) d_input, (void *) img->data,
+                                 sizeof(unsigned char) * img->width * img->height * img->channels,
+                                 cudaMemcpyHostToDevice));
+    CUDA_CHECK_RETURN(cudaMemcpy((void *) d_krn, (void *) krn->coefficients,
+                                 sizeof(unsigned long long int) * krn->size * krn->size,
+                                 cudaMemcpyHostToDevice));
+
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH);
+    dim3 gridDim(ceil(((float) img->width) / TILE_WIDTH), ceil(((float) img->height) / TILE_WIDTH));
+    int w = TILE_WIDTH + krn->size - 1;
+
+    kernel<<<gridDim, blockDim, sizeof(unsigned char) * w * w>>>(d_input, d_krn, d_output, img->height, img->width,
+                                                                 img->channels, krn->size,
+                                                                 krn->weight, w);
+    cudaDeviceSynchronize();
+
+    CUDA_CHECK_RETURN(cudaMemcpy((void *) res->data, (void *) d_output,
+                                 sizeof(unsigned char) * img->width * img->height * img->channels,
+                                 cudaMemcpyDeviceToHost));
+
+    cudaFree(d_input);
+    cudaFree(d_krn);
+    cudaFree(d_output);
+
+    return res;
+}
+
+#endif
+
+#if defined CONSTANT_MEMORY && defined SHARED_MEMORY
+#define TILE_WIDTH BLOCK_SIZE
+__constant__ unsigned long long int KERNEL[25 * 25];
+
+__global__ void
+kernel(unsigned char *input, unsigned char *output, int height,
+       int width, int channels,
+       int size, double weight, int w) {
+    // shared memory
+    extern __shared__ unsigned char input_d[];
+
+    // pixel to process
+    int iy = blockIdx.y * blockDim.y + threadIdx.y;
+    int ix = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // utility values
+    int kRadius = size / 2;
+    int batches = (w * w) / (TILE_WIDTH * TILE_WIDTH);
+
+    int dest, destY, destX, srcY, srcX;
+    unsigned long long int newVal;
+
+    for (int ic = 0; ic < channels; ic++) {
+        // shared memory loading
+        for (int b = 0; b < batches; b++) {
+            dest = threadIdx.y * TILE_WIDTH + threadIdx.x + b * TILE_WIDTH * TILE_WIDTH;
+            destY = dest / w;
+            destX = dest % w;
+            srcY = blockIdx.y * TILE_WIDTH + destY - kRadius;
+            srcX = blockIdx.x * TILE_WIDTH + destX - kRadius;
+
+            if (destY < w) {
+                if (srcX < 0 || srcX >= width) {        // edge handling: extend
+                    srcX = (srcX < 0) ? 0 : (width - 1);
+                }
+                if (srcY < 0 || srcY >= height) {
+                    srcY = (srcY < 0) ? 0 : (height - 1);
+                }
+
+                input_d[destY * w + destX] = input[srcY * width * channels + srcX * channels + ic];
+            }
+        }
+
+        __syncthreads();
+
+        // convolution
+        if (iy < height && ix < width) {
+            newVal = 0;
+            for (int ky = 0; ky < size; ky++) {
+                for (int kx = 0; kx < size; kx++) {
+                    newVal += (unsigned long long int) input_d[(threadIdx.y + ky) * w + (threadIdx.x + kx)] *
+                              KERNEL[ky * size + kx];
+                }
+            }
+
+            newVal = (unsigned long long int) ((long double) newVal * weight);
+            output[iy * width * channels + ix * channels + ic] = (unsigned char) newVal;
+        }
+        __syncthreads();
+    }
+}
+
+Image *process(Image *img, Kernel *krn) {
+    Image *res = Image_new_empty(img->width, img->height, img->channels);
+
+    unsigned char *d_input;
+    unsigned char *d_output;
+
+    CUDA_CHECK_RETURN(cudaMalloc((void **) &d_input, sizeof(unsigned char) * img->width * img->height * img->channels));
+    CUDA_CHECK_RETURN(
+            cudaMalloc((void **) &d_output, sizeof(unsigned char) * img->width * img->height * img->channels));
+
+    CUDA_CHECK_RETURN(cudaMemcpy((void *) d_input, (void *) img->data,
+                                 sizeof(unsigned char) * img->width * img->height * img->channels,
+                                 cudaMemcpyHostToDevice));
+    CUDA_CHECK_RETURN(cudaMemcpyToSymbol(KERNEL, (void *) krn->coefficients,
+                                         sizeof(unsigned long long int) * krn->size * krn->size));
+
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH);
+    dim3 gridDim(ceil(((float) img->width) / TILE_WIDTH), ceil(((float) img->height) / TILE_WIDTH));
+    int w = TILE_WIDTH + krn->size - 1;
+
+    kernel<<<gridDim, blockDim, sizeof(unsigned char) * w * w>>>(d_input, d_output, img->height, img->width,
+                                                                 img->channels, krn->size,
+                                                                 krn->weight, w);
+    cudaDeviceSynchronize();
+
+    CUDA_CHECK_RETURN(cudaMemcpy((void *) res->data, (void *) d_output,
+                                 sizeof(unsigned char) * img->width * img->height * img->channels,
+                                 cudaMemcpyDeviceToHost));
+
+    cudaFree(d_input);
+    cudaFree(d_output);
+
+    return res;
+}
+
 #endif
